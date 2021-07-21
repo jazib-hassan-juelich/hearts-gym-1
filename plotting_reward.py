481b160c2dbbba30d988722a82c8a8ce0751dccd
Jazib Hassan <63711255+jazib-hassan-juelich@users.noreply.github.com>
2021-07-21 15:47:09 +0200

    Added legend support

--- plotting_reward.py
+++ plotting_reward.py
@@ -14,11 +14,10 @@ for root,dirs,_ in os.walk(path):
         path_sub = os.path.join(root,d) # this is the current subfolder
         for filename in glob.glob(os.path.join(path_sub, '*.csv')):
             df = pd.read_csv(filename)
-            plt.plot(df['timesteps_total'],df['episode_reward_mean'])
+            plt.plot(df['timesteps_total'],df['episode_reward_mean'],label=filename[62:65])
             plt.xlabel('Total Time Steps')
             plt.ylabel('Epsiode Reward Mean')
-            #plt.legend([filename[70:80]])
-            plt.show()
-            #df.plot(x='timesteps_total',y='episode_reward_mean')
+            plt.legend()
+            #plt.show()
             name = os.path.split(filename)[1] # get the name of the current csv file
             #data_splitter(df, name)
